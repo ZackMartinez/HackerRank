be5dd1d1bfe635182d8aa1bbdbb1209f84fa5bc0
ZackMartinez <zacharytmartinez@gmail.com>
2017-05-19 16:22:20 -0500

    Apple Orange problem
    https://www.hackerrank.com/challenges/apple-and-orange

---
+++ AppleOrange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+	static class AppleOrange
+	{
+		internal static int NumberWithinHouseValues(this int[] fruitFallValues, int fruitTreeCenter, int[] houseBeginningAndEnd)
+		{
+			int fruitFallingOnHouse = fruitFallValues
+				.Where
+				(fruitFallValue =>
+					fruitTreeCenter + fruitFallValue >= houseBeginningAndEnd[0]
+				 && fruitTreeCenter + fruitFallValue <= houseBeginningAndEnd[1]
+				).Count();
+
+			return fruitFallingOnHouse;
+		}
+	}
+}
